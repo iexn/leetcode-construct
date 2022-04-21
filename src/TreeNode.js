87a26ea4929bd0cf9d4c50f798fce8ee17553233
iexn <iexn@qq.com>
2022-04-21 11:55:41 +0800

    Create TreeNode.js

---
+++ src/TreeNode.js
@@ -0,0 +1,63 @@
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
+function createTreeNode(data) {
+    if (data.length == 0) {
+        return new TreeNode(null);
+    }
+
+    let Tree = new TreeNode(data[0]);
+
+    if (data.length == 1) {
+        return Tree;
+    }
+
+    let current = [Tree];
+    let n = data.length;
+    let k = 1;
+    
+    while (k < n) {
+        let next = [];
+
+        for (let i = 0; i < current.length; i++) {
+            current[i].left = new TreeNode(data[k++]);
+            current[i].right = new TreeNode(data[k++]);
+
+            next.push(current[i].left, current[i].right);
+        }
+
+        current = next;
+    }
+
+    // 校验有效性
+    let next = [Tree];
+
+    while (next.length) {
+        let vil = [];
+
+        for (let i = 0; i < next.length; i++) {
+            if (next[i].left) {
+                if (next[i].left.val == null) {
+                    next[i].left = null;
+                } else {
+                    vil.push(next[i].left);
+                }
+            }
+
+            if (next[i].right) {
+                if (next[i].right.val == null) {
+                    next[i].right = null;
+                } else {
+                    vil.push(next[i].right);
+                }
+            }
+        }
+
+        next = vil;
+    }
+
+    return Tree;
+}
